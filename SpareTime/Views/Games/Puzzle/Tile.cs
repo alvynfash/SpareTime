d61a93ca0a37db94a7e7826aeaee8ed5e224608c
alvynfash <zetagik@gmail.com>
2017-05-17 15:53:47 +0200

    -new Tile class

---
+++ SpareTime/Views/Games/Puzzle/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SpareTime
+{
+	public class Tile
+	{
+		public enum DirectionFlag
+		{
+			Up,
+			Down,
+			Left,
+			Right,
+			None
+		}
+		public string Value { get; set; }
+		public int Row{ get; set; }
+		public int Column { get; set; }
+		public DirectionFlag Direction { get; set; }
+
+		public Tile()
+		{
+			
+		}
+
+		public async Task Move(DirectionFlag direction)
+		{
+			
+		}
+	}
+}
